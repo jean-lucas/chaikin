2140897d76add8c3076b0a19b6dc2e63f18e8952
jean lucas <jeanlpf@hotmail.com>
2021-04-10 01:22:08 +0200

    minor fixes

--- src/chaikin/chaikin_smoothing.py
+++ src/chaikin/chaikin_smoothing.py
@@ -5,7 +5,7 @@ openLine = [[0,0], [0, 1], [1, 1], [1.5, -0.5], [3, 2], [3,3]]
 closed = [[0,0], [0, 1], [1,1.5], [1, 0], [0, 0]]
 
 
-def _smooth(a) -> np.array:
+def _smooth(a: np.array) -> np.array:
     new_pts = []
     for w in windowed(a, 2):
         q = 0.75 * w[0] + 0.25 * w[1]
@@ -15,6 +15,15 @@ def _smooth(a) -> np.array:
 
 
 def smooth(a: np.array, n: 2) -> np.array:
+    if not isinstance(a, np.ndarray):
+        try:
+            a = np.array(a)
+        except:
+            raise Exception(f"Input must be a 2-d list or numpy array, but got: {a}")
+
+    if not len(a.shape) == 2 and a.shape[1] != 2:
+        raise Exception(f"Input must be a 2-d array")
+
     smoothed_pts = a
     for _ in range(n):
         smoothed_pts = _smooth(smoothed_pts)
